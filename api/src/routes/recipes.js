4035011759ff741c8f1d0b5ed4505d9ec74d2563
Eva <mac@macs-MacBook-Pro.local>
2021-09-29 08:38:41 -0300

    get casi listo

--- api/src/routes/recipes.js
+++ api/src/routes/recipes.js
@@ -29,7 +29,6 @@ const getDataAPI = async () => {
     const api = await axios.get(
       `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.apiKey}&addRecipeInformation=true&number=100`
     );
-    // console.log(api);
     const data = api.data.results.map((e) => {
       return {
         title: e.title,
@@ -47,22 +46,22 @@ const getDataAPI = async () => {
 };
 
 //desarrollador
-const getAllData = async () => {
-  const DBdata = await getDataDB();
-  return DBdata;
-};
 // const getAllData = async () => {
-//   const apiData = await getDataAPI();
 //   const DBdata = await getDataDB();
-//   const allData = apiData.concat(DBdata);
-//   return allData;
+//   return DBdata;
 // };
+const getAllData = async () => {
+  const apiData = await getDataAPI();
+  const DBdata = await getDataDB();
+  const allData = apiData.concat(DBdata);
+  return allData;
+};
 
-const searchByName = async (name) => {
+const searchByNameApi = async (name) => {
   // {YOUR_API_KEY}&addRecipeInformation=true&query=${nameQuery}&number=100
   try {
     const api = await axios.get(
-      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.apiKey}&addRecipeInformation=true&query=${name}`
+      `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.apiKey}&addRecipeInformation=true&titleMatch=${name}`
     );
     const data = api.data.results.map((e) => {
       return {
@@ -79,18 +78,63 @@ const searchByName = async (name) => {
     console.log(error, "errrrrror");
   }
 };
+const searchByNameDB = async (name) => {
+  await Recipe.findAll({
+    where: { name: name },
+  }); // terminar de armar la busqueda en la api
+};
+// https://api.spoonacular.com/recipes/complexSearch?apiKey=ade3e4d00939456cb63c10007d5bc40d&addRecipeInformation=true&query=pizza&number=100
 
 router.get("/", async function (req, res) {
   const allData = await getAllData();
-  const { name } = req.params;
+  const { name } = req.query;
   if (name) {
-    const dataByName = await searchByName();
-    if (dataByName) res.send(dataByName);
-  } else if (allData.length > 0) {
-    res.send(allData);
-  } else res.status(400).send("error no hay data");
+    /*sin usar el endpoint
+    const recipesByName = allData.filter((e) =>
+      e.name.toLowerCase().include(name.toLowerCase())
+    );*/
+    const recipesByNameApi = await searchByNameApi(name);
+    const recipesByNameDB = await searchByNameDB(name);
+    const allRecipesByName = recipesByNameApi.concat(recipesByNameDB);
+    if (allRecipesByName.length > 0) return res.send(allRecipesByName);
+    else res.send("There is no recipe whit the name provide");
+  } else if (allData.length > 0) res.send(allData);
+  else res.status(400).send("error no hay data");
 });
 
-router.get("/:idReceta", async (req, res) => {});
+//--------------------------------------------------
+
+const getByIdApi = async (id) => {
+  try {
+    const apiGetById = await axios.get(
+      `https://api.spoonacular.com/recipes/${id}/information?apiKey=${process.env.apiKey}`
+    );
+    const { data } = apiGetById;
+    const obj = {
+      title: data.title,
+      id: data.id,
+      vegetarian: data.vegetarian,
+      glutenFree: data.glutenFree,
+      dairyFree: data.dairyFree,
+      veryHealthy: data.veryHealthy,
+    };
+    return obj;
+  } catch (error) {
+    console.log(error.data, "algo mal");
+  }
+};
+const getbyIdDB = async (id) => {
+
+}
+
+router.get("/:idReceta", async (req, res) => {
+  const {idReceta} = req.params
+  const dataByIdDB = await getbyIdDB(idReceta)
+  if(!dataByIdDB){
+    const dataByIdApi = await getByIdApi(idReceta);
+    if (dataByIdApi) res.send(dataByIdApi);
+    else res.status(500).send("No data");
+  } else res.send(dataByIdDB);
+});
 
 module.exports = router;
